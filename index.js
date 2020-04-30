d509f3c00de6082649836a174b8d9a351e801418
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-30 19:12:00 +0200

    the function doesnt create a new object so the original object is updates by key and value

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ let driver = {}
 function updateDriverWithKeyAndValue(driver, key, value){
     
     const newDriver = {...driver};
-    
+    // create a function where a new object is instantiated including the original objects items {...object} so the original object is not destoyed
     newDriver[key] = value;
 
     return newDriver;
-}
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+};
 
