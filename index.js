2826717246dad9118021bb782a2d4d8b9858076e
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-04-30 19:08:23 +0200

    create a function where a new object is instantiated including the original objects items {...object} so the original object is not destoyed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+
+let driver = {}
+
+function updateDriverWithKeyAndValue(driver, key, value){
+    
+    const newDriver = {...driver};
+    
+    newDriver[key] = value;
+
+    return newDriver;
+}
+
