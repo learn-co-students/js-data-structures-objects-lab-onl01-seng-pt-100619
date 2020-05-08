28539464f0b21f08639f21ed38f6e4a01c3e1ca4
github username <github email address>
2020-05-07 21:44:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const driver = {}
+
+function updateDriverWithKeyAndValue(driverObj, key, value){
+    return Object.assign({}, driverObj, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value;
+    return driver;
+}
+
+function deleteFromDriverByKey(driverObj, key){
+    const newDriverObj = Object.assign({}, driverObj);
+    delete newDriverObj[key];
+    return newDriverObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driverObj, key){
+    delete driverObj[key] //why was key notation needed?
+    return driverObj
+}
